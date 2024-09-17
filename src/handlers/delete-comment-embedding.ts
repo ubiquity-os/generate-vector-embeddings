5366f26218a53cbd27692204dc5b3a70a49d6bdc
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2024-09-17 20:04:52 +0100

    chore: delete-comment-embeddings

---
+++ src/handlers/delete-comment-embedding.ts
@@ -0,0 +1,18 @@
+import { CallbackResult } from "../proxy-callbacks";
+import { Context } from "../types";
+
+export async function deleteCommentEmbedding(context: Context<"issue_comment.deleted">): Promise<CallbackResult> {
+  const {
+    logger,
+    adapters: { supabase },
+  } = context;
+
+  try {
+    await supabase.embeddings.deleteEmbedding(context.payload.comment.node_id);
+    logger.ok(`Successfully deleted comment!`);
+  } catch (error) {
+    throw error;
+  }
+
+  return { status: 200, reason: "success" };
+}
