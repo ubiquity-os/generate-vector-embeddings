59eccc1bd1d71d867abb2402b1b58e805c1127fc
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2024-09-17 20:05:59 +0100

    chore: update-task-embeddings

---
+++ src/handlers/update-task-embedding.ts
@@ -0,0 +1,17 @@
+import { CallbackResult } from "../proxy-callbacks";
+import { Context } from "../types";
+
+export async function updateTaskEmbedding(context: Context<"issues.edited">): Promise<CallbackResult> {
+  const {
+    logger,
+    adapters: { supabase },
+  } = context;
+  try {
+    const updated = await supabase.embeddings.updateConversationEmbeddings(context.payload.issue.node_id, context.payload, "task");
+    logger.ok(`Successfully updated issue!`, { updated });
+  } catch (error) {
+    throw error
+  }
+
+  return { status: 200, reason: "success" };
+}
