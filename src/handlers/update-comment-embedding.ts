1654da3ca206246df596287f200c13a868579e9c
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2024-09-17 20:05:06 +0100

    chore: update-comment-embeddings

---
+++ src/handlers/update-comment-embedding.ts
@@ -0,0 +1,21 @@
+import { CallbackResult } from "../proxy-callbacks";
+import { Context } from "../types";
+
+/**
+ * Updates embeddings for comments.
+ */
+export async function updateCommentEmbedding(context: Context<"issue_comment.edited">): Promise<CallbackResult> {
+  const {
+    logger,
+    adapters: { supabase },
+  } = context;
+
+  try {
+    const updated = await supabase.embeddings.updateConversationEmbeddings(context.payload.comment.node_id, context.payload, "comment");
+    logger.ok(`Successfully updated comment!`, { updated });
+  } catch (error) {
+    throw error;
+  }
+
+  return { status: 200, reason: "success" };
+}
