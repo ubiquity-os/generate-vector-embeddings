571c73b6b571d93b2cfc2069426abb8e09d0a29a
koya0 <namikoyaobi@proton.me>
2025-01-17 22:57:08 -0300

    fix: correct annotate call in commandHandler

--- src/handlers/user-annotate.ts
+++ src/handlers/user-annotate.ts
@@ -15,15 +15,16 @@ export async function commandHandler(context: Context) {
   if (context.command.name === "annotate") {
     const commentUrl = context.command.parameters.commentUrl ?? null;
     const scope = context.command.parameters.scope ?? "org";
+    let commentId = null;
     if (commentUrl) {
       const commentRegex = /#issuecomment-(\d+)$/;
       const match = commentUrl.match(commentRegex);
       if (!match) {
         throw logger.error("Invalid comment URL");
       }
-      const commentId = match[1];
-      await annotate(context, commentId, scope);
+      commentId = match[1];
     }
+    await annotate(context, commentId, scope);
   }
 }
 
